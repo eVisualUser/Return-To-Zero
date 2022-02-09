e0752bf3686917190818990230ba8a30166ce42f
eVisualUser <94703571+eVisualUser@users.noreply.github.com>
2022-02-09 09:19:22 +0100

    New conditions and actions

--- code/cmd.c
+++ code/cmd.c
@@ -5,7 +5,7 @@ int gen_new_luck()
 	return rand() % 6 + 1;
 }
 
-void execute_cmd(char* cmd, Page* page)
+void execute_cmd(char* cmd, Page* page, Player* player)
 {
 	if(str_match(str_crop(cmd), "debug"))
 		printf("DEBUG\n");
@@ -21,29 +21,237 @@ void execute_cmd(char* cmd, Page* page)
 	{
 		load_page_toml(page, get_first_page());
 	}
+	else if(str_match(str_crop(cmd), "doc"))
+	{
+		player->job = Doctor;
+	}
+	else if(str_match(str_crop(cmd), "eng"))
+	{
+		player->job = Engineer;
+	}
+	else if(str_match(str_crop(cmd), "geo"))
+	{
+		player->job = Geologist;
+	}
+	else if(str_match(str_crop(cmd), "+food"))
+	{
+		player->food++;
+	}
+	else if(str_match(str_crop(cmd), "-food"))
+	{
+		player->food--;
+	}
+	else if(str_match(str_crop(cmd), "+weight"))
+	{
+		player->weight++;
+	}
+	else if(str_match(str_crop(cmd), "-weight"))
+	{
+		player->weight--;
+	}
+	else if(str_match(str_crop(cmd), "-speed"))
+	{
+		player->speed--;
+	}
+	else if(str_match(str_crop(cmd), "+social"))
+	{
+		player->social++;
+	}
+	else if(str_match(str_crop(cmd), "-social"))
+	{
+		player->social--;
+	}
+	else if(str_match(str_crop(cmd), "+syringe"))
+	{
+		player->syringe++;
+	}
+	else if(str_match(str_crop(cmd), "-syringe"))
+	{
+		player->syringe--;
+	}
+	else if(str_match(str_crop(cmd), "+spanner"))
+	{
+		player->spanner++;
+	}
+	else if(str_match(str_crop(cmd), "-spanner"))
+	{
+		player->spanner--;
+	}
+	else if(str_match(str_crop(cmd), "+book"))
+	{
+		player->book++;
+	}
+	else if(str_match(str_crop(cmd), "-book"))
+	{
+		player->book--;
+	}
 }
 
 bool test_cond(char* cond, Player player, int luck)
 {
 	char* ccond = str_crop(cond);
 
-	if(str_match(ccond, COND_NONE)) return true;
-	if(str_match(ccond, COND_DOC)) 
+	int count = 0;
+	char** args = extract_args(ccond, &count);
+	for(int i = 0; i <= count; i++)
 	{
-		if(player.job == Doctor) return true;
-		else return false;
+		if(str_match(args[i], COND_NONE)) return true;
+		if(str_match(args[i], COND_DOC)) 
+		{
+			if(player.job == Doctor) return true;
+			else return false;
+		}
+		else if(str_match(args[i], COND_ENG)) 
+		{
+			if(player.job == Engineer) return true;
+			else return false;
+		}
+		else if(str_match(args[i], COND_GEO)) 
+		{
+			if(player.job == Geologist) return true;
+			else return false;
+		}
+		else if(str_match(args[i], "luck_high"))
+		{
+			if (luck >= 5)
+				return false;
+		}
+		else if(str_match(args[i], "luck_med"))
+		{
+			if (luck >= 3)
+				return false;
+		}
+		else if (str_match(args[i], "luck_low"))
+		{
+			if (luck >= 2)
+				return false;
+		}
+		else if (str_match(args[i], "!luck_high"))
+		{
+			if (luck <= 5)
+				return false;
+		}
+		else if (str_match(args[i], "!luck_med"))
+		{
+			if (luck <= 3)
+				return false;
+		}
+		else if (str_match(args[i], "!luck_low"))
+		{
+			if (luck <= 2)
+				return false;
+		}
+		else if (str_match(args[i], "food"))
+		{
+			if (player.food > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!food"))
+		{
+			if (player.food == 0)
+				return false;
+		}
+		else if (str_match(args[i], "weight"))
+		{
+			if (player.weight > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!weight"))
+		{
+			if(player.weight == 0)
+				return false;
+		}
+		else if (str_match(args[i], "speed"))
+		{
+			if(player.speed > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!speed"))
+		{
+			if(player.speed == 0)
+				return false;
+		}
+		else if (str_match(args[i], "social"))
+		{
+			if(player.weight > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!social"))
+		{
+			if(player.social == 0)
+				return false;
+		}
+		else if (str_match(args[i], "syringe"))
+		{
+			if(player.syringe > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!syringe"))
+		{
+			if(player.syringe == 0)
+				return false;
+		}
+		else if (str_match(args[i], "grapple_hook"))
+		{
+			if(player.grapple_hook > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!grapple_hook"))
+		{
+			if(player.grapple_hook == 0)
+				return false;
+		}
+		else if (str_match(args[i], "pickaxe"))
+		{
+			if(player.pickaxe > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!pickaxe"))
+		{
+			if(player.pickaxe == 0)
+				return false;
+		}
+		else if (str_match(args[i], "carbon_boots"))
+		{
+			if(player.carbon_boots == 0)
+				return false;
+		}
+		else if (str_match(args[i], "!carbon_boots"))
+		{
+			if(player.carbon_boots == 0)
+				return false;
+		}
+		else if (str_match(args[i], "flare"))
+		{
+			if(player.flare > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!flare"))
+		{
+			if(player.flare == 0)
+				return false;
+		}
+		else if (str_match(args[i], "spanner"))
+		{
+			if(player.spanner > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!spanner"))
+		{
+			if(player.spanner == 0)
+				return false;
+		}
+		else if (str_match(args[i], "book"))
+		{
+			if(player.book > 0)
+				return false;
+		}
+		else if (str_match(args[i], "!book"))
+		{
+			if(player.book == 0)
+				return false;
+		}
 	}
-	else if(str_match(ccond, COND_ENG)) 
-	{
-		if(player.job == Enginer) return true;
-		else return false;
-	}
-	else if(str_match(ccond, COND_GEO)) 
-	{
-		if(player.job == Geologist) return true;
-		else return false;
-	}
-	
 	return true;
 }
 
